c2ad7d4e8192154c713dbe1e5558167013dba9c9
Joohyuck <joohyuck.seo@gmail.com>
2020-10-21 14:51:39 +0900

    TextRPG 플레이어 생성

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -12,6 +12,12 @@ namespace TextRPG
             Mage = 3
         }
 
+        struct Player
+        {
+            public int hp;
+            public int attack;
+        }
+
         static ClassType ChooseClass()
         {
             Console.WriteLine("직업을 선택하세요!");
@@ -41,6 +47,32 @@ namespace TextRPG
             return choice;
         }
 
+        static void CreatePlayer(ClassType choice, out Player player)
+        {
+            // 기사(100/10), 궁수(75/12), 법사(50/15)
+            switch (choice)
+            {
+                case ClassType.Knight:
+                    player.hp = 100;
+                    player.attack = 10;
+                    break;
+
+                case ClassType.Archer:
+                    player.hp = 75;
+                    player.attack = 12;
+                    break;
+
+                case ClassType.Mage:
+                    player.hp = 50;
+                    player.attack = 15;
+                    break;
+
+                default:
+                    player.hp = 0;
+                    player.attack = 0;
+                    break;
+            }
+        }
 
         static void Main(string[] args)
         {
@@ -48,7 +80,17 @@ namespace TextRPG
             {
                 ClassType choice = ChooseClass();
                 if (choice != ClassType.None)
-                    break;
+                {
+                    // 캐릭터 생성
+                    Player player;
+
+                    CreatePlayer(choice, out player);
+
+                    Console.WriteLine($"HP{player.hp} Attack{player.attack}");
+
+                    // 필드로 가서 몬스터랑 싸운다
+
+                }
             }
         }
     }
