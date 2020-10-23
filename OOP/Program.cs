9cafa45603d6dd82a81604c287c02e978ba6e401
Joohyuck <joohyuck.seo@gmail.com>
2020-10-24 01:06:09 +0900

    복사(값)와 참조

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -8,10 +8,19 @@ namespace OOP
     // 속성: hp, attack
     // 기능: Move, Attack, Die
 
+    // Ref 참조
     class Knight
     {
         public int hp;
         public int attack;
+
+        public Knight Clone()
+        {
+            Knight knight = new Knight();
+            knight.hp = hp;
+            knight.attack = attack;
+            return knight;
+        }
 
         public void Move()
         {
@@ -24,18 +33,42 @@ namespace OOP
         }
     }
 
+    // 복사
+    struct Mage
+    {
+        public int hp;
+        public int attack;
+    }
 
     class Program
     {
+        static void KillMage(Mage mage)
+        {
+            mage.hp = 0;
+        }
+
+        static void KillKnight(Knight knight)
+        {
+            knight.hp = 0;
+        }
+
         static void Main(string[] args)
         {
-            Knight knight = new Knight();
+            Mage mage;
+            mage.hp = 100;
+            mage.attack = 50;
+            //KillMage(mage);
 
+            Mage mage2 = mage;
+            mage2.hp = 0;
+
+            Knight knight = new Knight();
             knight.hp = 100;
             knight.attack = 10;
+            //KillKnight(knight);
 
-            knight.Move();
-            knight.Attack();
+            Knight knight2 = knight.Clone();
+            knight.hp = 0;
         }
     }
 }
