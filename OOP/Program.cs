b1dbae129a57675f51ab7ad0daa305c45021686a
Joohyuck <joohyuck.seo@gmail.com>
2020-10-23 18:49:53 +0900

    객체지향의 시작

---
+++ OOP/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OOP
+{
+    // 객체(OOP: Object Oriented Programming)
+
+    // Knight
+    // 속성: hp, attack
+    // 기능: Move, Attack, Die
+
+    class Knight
+    {
+        public int hp;
+        public int attack;
+
+        public void Move()
+        {
+            Console.WriteLine("Knight Move");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Knight Attack");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+
+            knight.hp = 100;
+            knight.attack = 10;
+
+            knight.Move();
+            knight.Attack();
+        }
+    }
+}
