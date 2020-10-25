fab4e45c01bd55ad73559cf0cd53df66d4e61553
Joohyuck <joohyuck.seo@gmail.com>
2020-10-25 21:20:58 +0900

    상속성

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace OOP
 {
     // 객체(OOP: Object Oriented Programming)
-
-    // Knight
-    // 속성: hp, attack
-    // 기능: Move, Attack, Die
-
+    // 은닉성/상속성/다향성
     // Ref 참조
-    class Knight
+
+    class Player        // 부모 or 기반 클래스
     {
         // 필드
         static public int counter = 1;        // 오로지 1개만 존재!
@@ -18,35 +16,15 @@ namespace OOP
         public int hp;
         public int attack;
 
-        public Knight Clone()
+        public Player()
         {
-            Knight knight = new Knight();
-            knight.hp = hp;
-            knight.attack = attack;
-            return knight;
+            Console.WriteLine("Player 생성자 호출!");
         }
 
-        static public void Test()
+        public Player(int hp)
         {
-            counter++;
-        }
-
-        static public Knight CreateKnight()
-        {
-            Knight knight = new Knight();
-            knight.hp = 100;
-            knight.attack = 1;
-            return knight;
-        }
-
-        public Knight()
-        {
-            id = counter;
-            counter++;
-
-            hp = 100;
-            attack = 10;
-            Console.WriteLine("생성자 호출!");
+            this.hp = hp;
+            Console.WriteLine("Player hp 생성자 호출!");
         }
 
         public void Move()
@@ -60,17 +38,34 @@ namespace OOP
         }
     }
 
+    class Knight : Player        // 자식 or 파생 클래스
+    {
+        int c;
+
+        public Knight() : base(100)
+        {
+            this.c = 10;
+            base.hp = 100;
+            Console.WriteLine("Knight 생성자 호츨!");
+        }
+    }
+
+    class Mage : Player       
+    {
+
+    }
+
+    class Archer : Player
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Knight knight = Knight.CreateKnight();      // static
-            knight.Move();                              // 일반
-
-            Console.WriteLine();
-
-            Random rand = new Random();
-            rand.Next(0, 2);
+            Knight knight = new Knight();
+            knight.Move();
         }
     }
 }
