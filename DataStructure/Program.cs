ae85fc3546d5897a0b85e0eb2b572db92717df83
Joohyuck <joohyuck.seo@gmail.com>
2021-02-19 06:40:03 +0900

    List

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,53 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DataStructure
 {
     class Program
     {
-        class Map
-        {
-            int[,] tiles =
-            {
-                { 1, 1, 1, 1, 1 },
-                { 1, 0, 0, 0, 1 },
-                { 1, 0, 0, 0, 1 },
-                { 1, 0, 0, 0, 1 },
-                { 1, 1, 1, 1, 1 },
-            };
-
-            public void Render()
-            {
-                var defaultColor = Console.ForegroundColor;
-
-                for (int y = 0; y < tiles.GetLength(1); y++)
-                {
-                    for (int x = 0; x < tiles.GetLength(0); x++)
-                    {
-                        if (tiles[y, x] == 1)
-                            Console.ForegroundColor = ConsoleColor.Red;
-                        else
-                            Console.ForegroundColor = ConsoleColor.Green;
-
-                        Console.Write("\u25cf");
-                    }
-                    Console.WriteLine();
-                }
-                Console.ForegroundColor = defaultColor;
-            }
-        }
-
         static void Main(string[] args)
         {
-            Map map = new Map();
-            map.Render();
+            int[] arr = new int[10];
 
-            // 가변 배열
-            int[][] a = new int[3][];
-            a[0] = new int[3];
-            a[1] = new int[6];
-            a[2] = new int[2];
+            // List <- 동적 배열 [ 0 1 2 3 4 ]
+            List<int> list = new List<int>();
+            for(int i=0; i<5; i++)
+                list.Add(i);
 
-            a[0][0] = 1;
+            // 삽입 삭제
+            //list.Insert(2, 999);
+            //bool success = list.Remove(3);
+            //list.RemoveAt(0);
+            //list.Clear();
+
+            for (int i = 0; i < list.Count; i++)
+                Console.WriteLine(list[i]);
+
+            foreach(int num in list)
+                Console.WriteLine(num);
+
         }
     }
 }
