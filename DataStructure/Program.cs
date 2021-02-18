30c6aa065eb21d9ba83d253ca4f602cd30609ada
Joohyuck <joohyuck.seo@gmail.com>
2021-02-19 06:57:17 +0900

    Dictionary

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -3,29 +3,37 @@ using System.Collections.Generic;
 
 namespace DataStructure
 {
+    class Monster
+    {
+        public int id;
+
+        public Monster(int id) { this.id = id; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[10];
-
-            // List <- 동적 배열 [ 0 1 2 3 4 ]
             List<int> list = new List<int>();
-            for(int i=0; i<5; i++)
-                list.Add(i);
 
-            // 삽입 삭제
-            //list.Insert(2, 999);
-            //bool success = list.Remove(3);
-            //list.RemoveAt(0);
-            //list.Clear();
+            // HashTable
+            // 메모리 손해
+            // [메모리를 내주고, 성능을 취한다]
 
-            for (int i = 0; i < list.Count; i++)
-                Console.WriteLine(list[i]);
+            // Key -> Value
+            // Dictionary
+            Dictionary<int, Monster> dic = new Dictionary<int, Monster>();
 
-            foreach(int num in list)
-                Console.WriteLine(num);
+            for(int i=0; i<10000; i++)
+            {
+                dic.Add(i, new Monster(i));
+            }
+            
+            Monster mon;
+            bool found = dic.TryGetValue(20000, out mon);
 
+            dic.Remove(7777);
+            dic.Clear();
         }
     }
 }
